db426e18afef67db8c24253781668e901f756af2
jwong1MHS <jasonw5172001@gmail.com>
2021-12-12 00:41:26 -0500

    added hw2_4

--- HW2/sql/hw2_4.sql
+++ HW2/sql/hw2_4.sql
@@ -1,14 +1,25 @@
-WITH total_count AS (
-	SELECT cust, prod, sum(quant)
+WITH total_c AS (
+	SELECT cust, prod, sum(quant) total_q
 	FROM sales
 	GROUP BY cust, prod
 	ORDER BY cust, prod
 ),
-month_count AS (
-	SELECT cust, prod, sales.month, sum(quant)
+month_c AS (
+	SELECT cust, prod, sales.month, sum(quant) sum_m
 	FROM sales
 	GROUP BY cust, prod, sales.month
 	ORDER BY cust, prod, sales.month
+),
+month_agg AS (
+	SELECT m1.cust, m1.prod, m1.month, sum(m2.sum_m) sum_q
+	FROM month_c m1, month_c m2
+	WHERE m1.cust = m2.cust AND m1.prod = m2.prod AND m1.month >= m2.month
+	GROUP BY m1.cust, m1.prod, m1.month
+	ORDER BY m1.cust, m1.prod, m1.month
 )
-SELECT *
-FROm month_count
+SELECT month_agg.cust customer, month_agg.prod product, min(month_agg.month) "75% purchased by month"
+FROM month_agg, total_c
+WHERE month_agg.cust = total_c.cust AND month_agg.prod = total_c.prod
+		AND sum_q >= total_q * 0.75
+GROUP BY month_agg.cust, month_agg.prod
+ORDER BY month_agg.cust, month_agg.prod
